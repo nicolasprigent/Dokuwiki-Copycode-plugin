49dfd5bdf89fd6c3b9ab83a59649d4cf4697fb51
nicolasprigent <66468412+nicolasprigent@users.noreply.github.com>
2021-02-18 18:30:35 +0100

    Update lang.php

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -6,6 +6,6 @@
  * Translated by mamekedi
  */
 
-$lang['js']['Gekopieerd'] = 'De Code is gekopieerd!';
-$lang['js']['selectiegekopieerd'] = 'De geselecteerde Code is gekopieerd!';
-$lang['js']['Error'] = 'Er is iets fout gegaan met het kopiëren van de Code.';
+$lang['js']['copied'] = 'De Code is gekopieerd!';
+$lang['js']['selectioncopied'] = 'De geselecteerde Code is gekopieerd!';
+$lang['js']['error'] = 'Er is iets fout gegaan met het kopiëren van de Code.';
