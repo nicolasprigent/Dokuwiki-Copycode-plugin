3132d5580f422f8b0cc3be37eba1cc5fd5d00928
nicolasprigent <66468412+nicolasprigent@users.noreply.github.com>
2021-02-18 14:26:44 +0100

    Create lang.php
    added dutch language, thanks to mamekedi

---
+++ lang/nl/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Dutch language file for copycode plugin
+ *
+ * @author Nicolas Prigent <mail.nicolasprigent@gmail.com>
+ * Translated by mamekedi
+ */
+
+$lang['js']['Gekopieerd'] = 'De Code is gekopieerd!';
+$lang['js']['selectiegekopieerd'] = 'De geselecteerde Code is gekopieerd!';
+$lang['js']['Error'] = 'Er is iets fout gegaan met het kopiëren van de Code.';
