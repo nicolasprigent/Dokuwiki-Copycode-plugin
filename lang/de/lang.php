754a00436e39354d4ed45ccd422075a94a9e359a
Stefan Lück <slueck@pp-software.de>
2021-04-21 18:16:29 +0200

    Create lang.php

---
+++ lang/de/lang.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * German language file for copycode plugin
+ *
+ * @author Nicolas Prigent <mail.nicolasprigent@gmail.com>
+ * Translated by footstark
+ */
+
+$lang['js']['copied'] = 'Code wurde kopiert!';
+$lang['js']['selectioncopied'] = 'Ausgewählter Code wurde kopiert!';
+$lang['js']['error'] = 'Beim Kopieren des Codes ist etwas schief gelaufen.';
