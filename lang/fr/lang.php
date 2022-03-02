b8fe5fc69da2f02604b64732845e1d901eb5085b
nicolasprigent <66468412+nicolasprigent@users.noreply.github.com>
2022-03-02 16:03:49 +0100

    Adds "fr" translation for inline copy

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -6,5 +6,6 @@
  */
 
 $lang['js']['copied'] = 'Le code a été copié !';
+$lang['js']['copiedinline'] = 'Le code a été copié sur une ligne.';
 $lang['js']['selectioncopied'] = 'La sélection de code a été copiée !';
 $lang['js']['error'] = 'Quelque chose a empêché la copie du code.';
