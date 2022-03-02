50173f9ed67b0971f49b45ac4126f3767a061f0a
nicolasprigent <66468412+nicolasprigent@users.noreply.github.com>
2022-03-02 16:00:01 +0100

    Update script.js
    - Adds functionnality to remove extra non ascii character bug
    - Adds functionnality and code logic for mouse actions
    - Adds inline copy on right click
    - Removing default contextmenu on right click on a code block
    - Better writing of jquery elements (using $ instead of repetitive jQuery functions)

--- script.js
+++ script.js
@@ -1,88 +1,122 @@
 /**
-
  * DokuWiki Plugin copycode (Action Component)
-
  *
-
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
-
  * @author  Nicolas Prigent <mail.nicolasprigent@gmail.com>
-
- * 
-
+ *
  * Adds a click event on all code blocks that copy the content of the block to clipboard
-
- * 
-
+ *
  */
 
+jQuery(document).ready(function ($) {
+  //Function to copy the content of an element and send it to clipboard
+  function writeToClipboard(elem, mc) {
+    inputValue = "";
 
-
-document.addEventListener('DOMContentLoaded', function () {
-
-    var bloc_code = jQuery("#dokuwiki__content pre.code, #dokuwiki__content pre.file");
-	
-	if (JSINFO.plugins.copycode.EnableForInline)
-		bloc_code = bloc_code.add("#dokuwiki__content code");
-
-	bloc_code.addClass("enabled-copycode");
-	
-    for(i=0;i<bloc_code.length;i++){
-
-        bloc_code[i].addEventListener('click', function(){
-            selected_text = window.getSelection().toString();
-            if (selected_text != "") {
-                writeToClipboard(selected_text);
-            }else{
-                writeToClipboard(this);
-            }
-        });
-
-        line = jQuery(bloc_code[i]).find("ol > li").append('<span class="copycode_line">_||copycode||_</span>');
-
-    }
-
-});
-
-
-function writeToClipboard(elem) {
-    inputValue = '';
-    if (typeof elem == 'string'){
-        inputValue = elem;
-        alertText = 'selectioncopied';
-        alertClass = 'orange';
-    }else{
-        inputValue = elem.textContent;
-        if (inputValue) {
-            inputValue = inputValue.split("_||copycode||_").join("\n");
-            alertText = 'copied';
-            alertClass = 'green';
-        }
-    }
-    if (inputValue != '') {
-        if (navigator.clipboard != undefined) {
-            navigator.clipboard.writeText(inputValue);
+    if (typeof elem == "string") {
+      inputValue = elem;
+      alertText = "selectioncopied";
+      alertClass = "orange";
+	    
+    } else {
+      inputValue = elem.textContent;
+	    
+      if (inputValue) {
+        if (mc == 3) {
+          inputValue = inputValue.split("_||copycode||_").join(" ");
+          inputValue = inputValue.split("\n").join(" ");
+          alertText = "copiedinline";
+          alertClass = "blue";
+		
         } else {
-            /*if for any reason the clipboard is unavalaible, uses the fake textarea hack to copy the content*/
-            var $textarea = jQuery('<textarea />');
-            $textarea.val(inputValue).css({ width: "1px", height: "1px" }).appendTo('body');
-            $textarea.select();
-            document.execCommand('copy');
-            $textarea.remove();
+          inputValue = inputValue.split("_||copycode||_").join("\n");
+          alertText = "copied";
+          alertClass = "green";
         }
-        alertMessage(LANG.plugins.copycode[alertText], alertClass);
-    } 
-};
+      }
+    }
 
+    //replacing problematic white space character that appears for no obvious reason :/
+    inputValue = inputValue.split(/\u00A0/).join("");
+	  
+    if (inputValue != "") {
+      //check if clipboard is available in navigator
+      if (navigator.clipboard != undefined) {
+        //Copy raw text to clipboard
+        navigator.clipboard.writeText(inputValue);
+      } else {
+        //if for any reason the clipboard is unavalaible, uses the fake textarea hack to copy the content
 
-function alertMessage(message, alertclass){
-    var alertMsg = '<div class="' + alertclass + ' alert-copycode">' + message + '</div>';
+        var $textarea = $("<textarea />");
 
-    jQuery( "body" ).append( alertMsg );
+        $textarea
+          .val(inputValue)
+          .css({ width: "1px", height: "1px" })
+          .appendTo("body");
 
-    window.setTimeout(function() {
-        jQuery(".alert-copycode").fadeTo(500, 0).slideUp(500, function(){
-            jQuery(this).remove(); 
+        $textarea.select();
+
+        document.execCommand("copy");
+
+        $textarea.remove();
+      }
+
+      alertMessage(LANG.plugins.copycode[alertText], alertClass);
+    }
+  }
+
+  function alertMessage(message, alertclass) {
+    var alertMsg =
+      '<div class="' + alertclass + ' alert-copycode">' + message + "</div>";
+
+    $("body").append(alertMsg);
+
+    window.setTimeout(function () {
+      $(".alert-copycode")
+        .fadeTo(500, 0)
+        .slideUp(500, function () {
+          $(this).remove();
         });
     }, 1000);
-};
+  }
+
+  var bloc_code = $("#dokuwiki__content pre.code, #dokuwiki__content pre.file");
+
+  if (JSINFO.plugins.copycode.EnableForInline)
+    bloc_code = bloc_code.add("#dokuwiki__content code");
+
+  bloc_code.addClass("enabled-copycode");
+
+  for (i = 0; i < bloc_code.length; i++) {
+    //deactivate context menu on right click
+    $(bloc_code[i]).on("contextmenu", function (evt) {
+      evt.preventDefault();
+    });
+    $(bloc_code[i]).mouseup(function (event) {
+      switch (event.which) {
+        case 1:
+          selected_text = window.getSelection().toString();
+
+          if (selected_text != "") {
+            writeToClipboard(selected_text, 1);
+          } else {
+            writeToClipboard(this, 1);
+          }
+          break;
+        case 2:
+          //alert("Middle Mouse button");
+          break;
+        case 3:
+          //alert("Middle Mouse button");
+          writeToClipboard(this, 3);
+          break;
+        default:
+          alert("Nothing");
+      }
+    });
+
+    line = $(bloc_code[i])
+      .find("ol > li")
+      .append('<span class="copycode_line">_||copycode||_</span>');
+  }
+});
