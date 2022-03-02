dc4b830376be0488bbd55703577daf80a4a1941c
nicolasprigent <66468412+nicolasprigent@users.noreply.github.com>
2022-03-02 16:04:26 +0100

    Adds "nl" translation for inline copy
    deepl automatic translation, may need correction

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -7,5 +7,6 @@
  */
 
 $lang['js']['copied'] = 'De Code is gekopieerd!';
+$lang['js']['copiedinline'] = 'De code is gekopieerd naar een regel.';
 $lang['js']['selectioncopied'] = 'De geselecteerde Code is gekopieerd!';
 $lang['js']['error'] = 'Er is iets fout gegaan met het kopiëren van de Code.';
